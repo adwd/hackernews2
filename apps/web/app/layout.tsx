831bd66ef3a0de955dda02ccfe72d9b366e232c8
adwd <ecdt.mn@gmail.com>
2023-10-17 15:33:49 +0900

    fix lint

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter as FontSans } from "next/font/google";
 import { cn } from "../lib/utils";
 import { ThemeProvider } from "../components/theme-provider";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
@@ -30,8 +30,8 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
-          enableSystem
           disableTransitionOnChange
+          enableSystem
         >
           {children}
         </ThemeProvider>
